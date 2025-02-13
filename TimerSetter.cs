613c53e364eaa415c1e9e0fd9002602966d1b624
ryangregoire1999 <ryangregoire286@gmail.com>
2025-02-12 22:09:27 -0500

    Add project files.

---
+++ TimerSetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TimeSetter.Maths;
+
+namespace TimeSetter
+{
+    public class TimerSetter
+    {
+        Maths.Objects m = new Maths.Objects();
+
+    }
+
+
+}
---
+++ Timing/Timing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSetter.Timing
+{
+    class Timing
+    {
+        public int Timer = 0;
+
+        public int setTimerHours(int h, int? m, int? s )
+        {
+            return 60 * h;
+        } 
+
+        public int getTimer(int time)
+        {
+            return this.Timer = time;
+        }
+
+
+
+    }
+}
